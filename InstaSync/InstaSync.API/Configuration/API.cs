2ea9c09e386a90cdbd0922fd1904c5be7ab920ab
DESKTOP-47CKMKT\Alexander <at.webdes@gmail.com>
2016-11-28 22:27:20 -0500

    Removed custom binder

--- InstaSync/InstaSync.API/Configuration/API.cs
+++ InstaSync/InstaSync.API/Configuration/API.cs
@@ -16,9 +16,17 @@ namespace InstaSync.API.Configuration
 
         static Api()
         {
-            Config.ClientId = "8a03f259c13847e7883805602cca6b0e";
-            Auth.AccessToken = "3047143526.1fb234f.4df2493fb853495785173e30b6c60970 ";
-            Auth.User = new UserInfo { Id = 3047143526 };
+            //Config.ClientId = "a34ef1b512094db788de0b2ccab900ac";
+            //Auth.AccessToken = "2097338967.1fb234f.7f6a2393717c4b419d685b905d801447";
+            //Auth.User = new UserInfo { Id = 2097338967 };
+            //Config.ClientId = "8a03f259c13847e7883805602cca6b0e";
+            //Auth.AccessToken = "3047143526.1fb234f.4df2493fb853495785173e30b6c60970 ";
+            //Auth.User = new UserInfo { Id = 3047143526 };
+            
+            ///"username": "instasynsapp", pass: 13june13, e-mail:  tsemalex@gmail.com
+            Config.ClientId = "d23af31957f4425bb596949d838f9c53";
+            Auth.AccessToken = "3612683455.d23af31.983d947054b4456ebfa6bc36330c4657";
+            Auth.User = new UserInfo { Id = 3612683455 };
         }
     }
 }
